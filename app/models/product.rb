3a1d23f9d1a4b3022a005e390f523131b2a493c2
ndiazpi <ndiaz@logicsolutions.es>
2025-03-09 20:04:33 +0100

    feat: Create models and migrations for Cart and CartItem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    has_many :cart_items
+    has_many :carts, through: :cart_items
+  
+    validates :code, presence: true, uniqueness: true
+    validates :name, presence: true
+    validates :price, presence: true, numericality: { greater_than: 0 }
 end
+  
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_09_183423) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_09_185515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -22,4 +37,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_09_183423) do
     t.decimal "price", precision: 8, scale: 2, null: false
   end
 
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
 end
